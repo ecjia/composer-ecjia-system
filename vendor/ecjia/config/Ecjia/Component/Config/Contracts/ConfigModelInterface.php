0bcaaeab5bef8572784b09e956722795648b9f5f
王正东 <wangzhengdong@ecmoban.com>
2019-07-31 17:07:25 +0800

    更新代码

--- vendor/ecjia/config/Ecjia/Component/Config/Contracts/ConfigModelInterface.php
+++ vendor/ecjia/config/Ecjia/Component/Config/Contracts/ConfigModelInterface.php
@@ -4,4 +4,15 @@ namespace Ecjia\Component\Config\Contracts;
 interface ConfigModelInterface
 {
 
+
+    /**
+     * 写入数据库数据
+     * @param string $key
+     * @param string $value
+     */
+    public function writeItem($key, $value);
+
+
+
+
 }
