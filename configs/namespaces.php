7e395e92e741b3b914dc9d166b7bc09918284322
王正东 <wangzhengdong@ecmoban.com>
2019-08-01 15:40:34 +0800

    添加命名空间

--- configs/namespaces.php
+++ configs/namespaces.php
@@ -4,6 +4,7 @@ $vendorDir = realpath(__DIR__ . '/../vendor');
 
 return array(
 	'Ecjia\Component\Config' => $vendorDir . '/ecjia/config/Ecjia/Component/Config',
+	'Ecjia\Component\AdminLog' => $vendorDir . '/ecjia/admin-log/Ecjia/Component/AdminLog',
 
 
 );
