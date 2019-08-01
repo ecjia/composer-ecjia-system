b76d21528d16d87562ea7493137073f1295db004
王正东 <wangzhengdong@ecmoban.com>
2019-08-01 16:06:42 +0800

    更新命名空间

--- configs/namespaces.php
+++ configs/namespaces.php
@@ -5,6 +5,8 @@ $vendorDir = realpath(__DIR__ . '/../vendor');
 return array(
 	'Ecjia\Component\Config' => $vendorDir . '/ecjia/config/Ecjia/Component/Config',
 	'Ecjia\Component\AdminLog' => $vendorDir . '/ecjia/admin-log/Ecjia/Component/AdminLog',
+	'Ecjia\Component\ApiClient' => $vendorDir . '/ecjia/api-client/Ecjia/Component/ApiClient',
+	'Ecjia\Component\ApiServer' => $vendorDir . '/ecjia/api-server/Ecjia/Component/ApiServer',
 
 
 );
