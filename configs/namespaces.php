d11332c9d982b86b3692bde60f05c12313fefe65
王正东 <wangzhengdong@ecmoban.com>
2019-08-16 13:16:20 +0800

    优化代码

--- configs/namespaces.php
+++ configs/namespaces.php
@@ -8,13 +8,13 @@ return array(
     'Ecjia\Component\ApiClient'   => $vendorDir . '/ecjia/api-client/Ecjia/Component/ApiClient',
     'Ecjia\Component\ApiServer'   => $vendorDir . '/ecjia/api-server/Ecjia/Component/ApiServer',
     'Ecjia\Component\AppClient'   => $vendorDir . '/ecjia/app-client/Ecjia/Component/AppClient',
-    'Ecjia\Component\FileHash'   => $vendorDir . '/ecjia/file-hash/Ecjia/Component/FileHash',
+    'Ecjia\Component\FileHash'    => $vendorDir . '/ecjia/file-hash/Ecjia/Component/FileHash',
     'Ecjia\Component\ShowMessage' => $vendorDir . '/ecjia/show-message/Ecjia/Component/ShowMessage',
     'Ecjia\Component\CleanCache'  => $vendorDir . '/ecjia/clean-cache/Ecjia/Component/CleanCache',
     'Ecjia\Component\EcjiaCloud'  => $vendorDir . '/ecjia/ecjia-cloud/Ecjia/Component/EcjiaCloud',
     'Ecjia\Component\EcjiaOpen'   => $vendorDir . '/ecjia/ecjia-open/Ecjia/Component/EcjiaOpen',
     'Ecjia\Component\OpenType'    => $vendorDir . '/ecjia/open-type/Ecjia/Component/OpenType',
-    'Ecjia\Component\Theme'    => $vendorDir . '/ecjia/theme/Ecjia/Component/Theme',
+    'Ecjia\Component\Theme'       => $vendorDir . '/ecjia/theme/Ecjia/Component/Theme',
 
 
 );
