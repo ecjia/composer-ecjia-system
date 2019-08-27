be3d32e634e0520cf4cea3423e5ebd8392c71ca4
王正东 <wangzhengdong@ecmoban.com>
2019-08-27 19:11:49 +0800

    封装HooKerServiceProvider

---
+++ classes/Hookers/AddMacroSendMailAction.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Ecjia\System\Hookers;
+
+
+use Ecjia\System\Frameworks\Component\Mailer;
+use RC_Mail;
+
+class AddMacroSendMailAction
+{
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param $event
+     * @return void
+     */
+    public function handle()
+    {
+        RC_Mail::macro('send_mail', function ($name, $email, $subject, $content, $type = 0, $notification = false) {
+            return with(new Mailer($this))->send_mail($name, $email, $subject, $content, $type, $notification);
+        });
+
+    }
+
+}
---
+++ classes/Providers/HooKerServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Ecjia\System\Providers;
+
+class HooKerServiceProvider
+{
+    /**
+     * The hook action listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $actions = [
+//        'royalcms.query' => [
+//            'Ecjia\System\Listeners\DatabaseQueryListener',
+//        ],
+//
+//        'royalcms.warning.exception' => [
+//            'Ecjia\System\Listeners\WarningExceptionListener',
+//        ],
+    ];
+
+    /**
+     * The hook filters listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $filters = [];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Royalcms\Component\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot($events)
+    {
+
+        foreach ($this->actions as $event => $listeners) {
+            foreach ($listeners as $listener) {
+                $events->listen($event, $listener);
+            }
+        }
+
+        foreach ($this->filters as $event => $listeners) {
+            foreach ($listeners as $listener) {
+                $events->listen($event, $listener);
+            }
+        }
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the events and handlers.
+     *
+     * @return array
+     */
+    public function actions()
+    {
+        return $this->actions;
+    }
+
+    /**
+     * Get the events and handlers.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return $this->filters;
+    }
+
+}
