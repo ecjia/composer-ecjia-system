7f995e0fa68f9a545747d7e7bbb6cfafa6b37479
王正东 <wangzhengdong@ecmoban.com>
2019-08-27 18:51:45 +0800

    添加数据库查询日志监听对象

---
+++ classes/Listeners/DatabaseQueryListener.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Ecjia\System\Listeners;
+
+
+use RC_Logger;
+
+class DatabaseQueryListener
+{
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    /**
+     * Handle the event.
+     *
+     * @param $event
+     * @return void
+     */
+    public function handle($query, $bindings, $time)
+    {
+        if (config('system.debug')) {
+            $query = str_replace('?', '"'.'%s'.'"', $query);
+            $sql = vsprintf($query, $bindings);
+            RC_Logger::getLogger(RC_Logger::LOG_SQL)->info('sql:'.$sql);
+        }
+
+    }
+
+}
