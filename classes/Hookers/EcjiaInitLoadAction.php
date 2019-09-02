9f3b118c7ae98b44f2d8dc08b5e19f5016b63eea
王正东 <wangzhengdong@ecmoban.com>
2019-09-02 14:07:16 +0800

    移除转移后的废弃代码

--- classes/Hookers/EcjiaInitLoadAction.php
+++ classes/Hookers/EcjiaInitLoadAction.php
@@ -41,9 +41,6 @@ class EcjiaInitLoadAction
         RC_Loader::load_sys_func('deprecated');
         RC_Loader::load_sys_func('extention');
 
-//        RC_Hook::add_filter('set_server_timezone', array('ecjia', 'current_timezone'));
-//        RC_Hook::add_action('admin_print_main_bottom', array('ecjia', 'echo_query_info'), 99);
-
         $rc_script = RC_Script::instance();
         $rc_style = RC_Style::instance();
         ecjia_loader::default_scripts($rc_script);
