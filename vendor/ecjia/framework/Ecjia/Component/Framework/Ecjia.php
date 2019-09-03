5b713d597717377daaa14323195692f767a8c634
王正东 <wangzhengdong@ecmoban.com>
2019-09-03 20:20:21 +0800

    移除hook事件类中

--- vendor/ecjia/framework/Ecjia/Component/Framework/Ecjia.php
+++ vendor/ecjia/framework/Ecjia/Component/Framework/Ecjia.php
@@ -222,16 +222,6 @@ class Ecjia extends Container
         return with(new \Ecjia\System\Frameworks\Component\CharCode)->byText();
     }
 
-    public static function loadGlobalPlugins()
-    {
-        $global_plugins = ecjia_config::instance()->get_addon_config('global_plugins', true);
-        if (is_array($global_plugins)) {
-            foreach ($global_plugins as $plugin_file) {
-                RC_Plugin::load_files($plugin_file);
-            }
-        }
-    }
-
     /**
      * @return bool
      */
