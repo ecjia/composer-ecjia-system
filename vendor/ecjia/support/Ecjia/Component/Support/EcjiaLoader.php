8dae65993318c55c172e2c4546d04a7406d0a088
王正东 <wangzhengdong@ecmoban.com>
2019-10-12 13:05:11 +0800

    ecjia_loader类提升为support组件

--- vendor/ecjia/support/Ecjia/Component/Support/EcjiaLoader.php
+++ vendor/ecjia/support/Ecjia/Component/Support/EcjiaLoader.php
@@ -44,25 +44,24 @@
 //
 //  ---------------------------------------------------------------------------------
 //
-defined('IN_ECJIA') or exit('No permission resources.');
+namespace Ecjia\Component\Support;
+
+use RC_Hook;
 
 /**
  * ecjia scripts and styles default loader.
- *
  * Several constants are used to manage the loading, concatenating and compression of scripts and CSS:
  * define('SCRIPT_DEBUG', true); loads the development (non-minified) versions of all scripts and CSS, and disables compression and concatenation,
  * define('CONCATENATE_SCRIPTS', false); disables compression and concatenation of scripts and CSS,
  * define('COMPRESS_SCRIPTS', false); disables compression of scripts,
  * define('COMPRESS_CSS', false); disables compression of CSS,
  * define('ENFORCE_GZIP', true); forces gzip for compression (default is deflate).
- *
  * The globals $concatenate_scripts, $compress_scripts and $compress_css can be set by plugins
  * to temporarily override the above settings. Also a compression test is run once and the result is saved
  * as option 'can_compress_scripts' (0/1). The test will run again if that option is deleted.
- *
  * @package ecjia
  */
-class ecjia_loader
+class EcjiaLoader
 {
 
     /**
