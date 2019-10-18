0fb4f1e583064ffd73e01b67819c7ffbf0241451
王正东 <wangzhengdong@ecmoban.com>
2019-10-18 11:04:38 +0800

    移除Ecjia\Component命名空间自定义

--- configs/namespaces.php
+++ configs/namespaces.php
@@ -3,35 +3,7 @@
 $vendorDir = realpath(__DIR__ . '/../vendor');
 
 return array(
-    'Ecjia\\Component\\Framework\\'    => $vendorDir . '/ecjia/framework/Ecjia/Component/Framework',
-    'Ecjia\\Component\\Config\\'       => $vendorDir . '/ecjia/config/Ecjia/Component/Config',
-    'Ecjia\\Component\\AdminLog\\'     => $vendorDir . '/ecjia/admin-log/Ecjia/Component/AdminLog',
-    'Ecjia\\Component\\ApiClient\\'    => $vendorDir . '/ecjia/api-client/Ecjia/Component/ApiClient',
-    'Ecjia\\Component\\ApiServer\\'    => $vendorDir . '/ecjia/api-server/Ecjia/Component/ApiServer',
-    'Ecjia\\Component\\AppClient\\'    => $vendorDir . '/ecjia/app-client/Ecjia/Component/AppClient',
-    'Ecjia\\Component\\FileHash\\'     => $vendorDir . '/ecjia/file-hash/Ecjia/Component/FileHash',
-    'Ecjia\\Component\\ShowMessage\\'  => $vendorDir . '/ecjia/show-message/Ecjia/Component/ShowMessage',
-    'Ecjia\\Component\\CleanCache\\'   => $vendorDir . '/ecjia/clean-cache/Ecjia/Component/CleanCache',
-    'Ecjia\\Component\\Database\\'     => $vendorDir . '/ecjia/database/Ecjia/Component/Database',
-    'Ecjia\\Component\\EcjiaCloud\\'   => $vendorDir . '/ecjia/ecjia-cloud/Ecjia/Component/EcjiaCloud',
-    'Ecjia\\Component\\EcjiaLicense\\' => $vendorDir . '/ecjia/ecjia-license/Ecjia/Component/EcjiaLicense',
-    'Ecjia\\Component\\EcjiaOpen\\'    => $vendorDir . '/ecjia/ecjia-open/Ecjia/Component/EcjiaOpen',
-    'Ecjia\\Component\\OpenType\\'     => $vendorDir . '/ecjia/open-type/Ecjia/Component/OpenType',
-    'Ecjia\\Component\\Theme\\'        => $vendorDir . '/ecjia/theme/Ecjia/Component/Theme',
-    'Ecjia\\Component\\App\\'          => $vendorDir . '/ecjia/app/Ecjia/Component/App',
-    'Ecjia\\Component\\Plugin\\'       => $vendorDir . '/ecjia/plugin/Ecjia/Component/Plugin',
-    'Ecjia\\Component\\View\\'         => $vendorDir . '/ecjia/view/Ecjia/Component/View',
-    'Ecjia\\Component\\Widget\\'       => $vendorDir . '/ecjia/widget/Ecjia/Component/Widget',
-    'Ecjia\\Component\\Option\\'       => $vendorDir . '/ecjia/option/Ecjia/Component/Option',
-    'Ecjia\\Component\\Metadata\\'     => $vendorDir . '/ecjia/metadata/Ecjia/Component/Metadata',
-    'Ecjia\\Component\\Notification\\' => $vendorDir . '/ecjia/notification/Ecjia/Component/Notification',
-    'Ecjia\\Component\\Editor\\'       => $vendorDir . '/ecjia/editor/Ecjia/Component/Editor',
-    'Ecjia\\Component\\Screen\\'       => $vendorDir . '/ecjia/screen/Ecjia/Component/Screen',
-    'Ecjia\\Component\\Purview\\'      => $vendorDir . '/ecjia/purview/Ecjia/Component/Purview',
-    'Ecjia\\Component\\Support\\'      => $vendorDir . '/ecjia/support/Ecjia/Component/Support',
-    'Ecjia\\Component\\UpgradeCheck\\' => $vendorDir . '/ecjia/upgrade-check/Ecjia/Component/UpgradeCheck',
-
-
+    
 );
 
 //end
