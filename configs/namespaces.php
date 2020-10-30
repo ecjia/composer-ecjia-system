8ff03f63eff1baffc1be721f39f6bfa3d463873b
王正东 <wangzhengdong@ecmoban.com>
2020-10-30 09:09:52 +0800

    设置admin-frameworks命名空间

--- configs/namespaces.php
+++ configs/namespaces.php
@@ -5,7 +5,8 @@ $systemDir = realpath(__DIR__ . '/../');
 
 return array(
     'Ecjia\System\AdminPanel' => $systemDir . '/admin-panel/src',
-    'Ecjia\System\AdminUI' => $systemDir . '/admin-ui/src',
+    'Ecjia\System\AdminUI'    => $systemDir . '/admin-ui/src',
+    'Ecjia\System\Frameworks' => $systemDir . '/admin-frameworks/src',
 );
 
 //end
