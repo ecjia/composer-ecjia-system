e3538a250841184ccedaa83879462505c6704592
王正东 <wangzhengdong@ecmoban.com>
2020-11-19 18:29:59 +0800

    后台中间件选项开启

--- configs/middlewares.php
+++ configs/middlewares.php
@@ -2,7 +2,7 @@
 
 return [
     \Ecjia\System\Middleware\DebugDisplayQueryMiddleware::class,
-    // \Ecjia\System\Middleware\XFrameOptionsMiddleware::class,
+//    \Ecjia\System\Middleware\XFrameOptionsMiddleware::class,
     \Ecjia\System\Middleware\XXSSProtectionMiddleware::class,
     \Ecjia\System\Middleware\AdminCheckLoginRequest::class,
 ];
